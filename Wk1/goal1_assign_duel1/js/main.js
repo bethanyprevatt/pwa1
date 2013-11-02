77f23b2caaf6b566f04305903ed36d8325cee590
Bethany Prevatt <bethany.prevatt@fullsail.edu>
2013-11-02 16:26:52 +0900

    varsetup

---
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,37 @@
+/*
+Name: Bethany Prevatt
+Date: November 2, 2013
+Assignment: The Duel - Part 1 
+*/
+
+//self-executing function
+(function(){
+
+	console.log("FIGHT!!!");
+	
+	//Player Name
+	var playerOneName = "Spiderman";
+	var playerTwoName = "Batman";
+	
+	//player damage
+	var player1Damage = 30;
+	var player2Damage = 30;
+	
+	//player health
+	var playerOneHealth = 100;
+	var playerTwoHealth = 100;
+	
+	var round = 1;
+	
+	function fight(){};
+	
+	function winnerCheck(){};
+	
+//program starts here
+	
+	fight();
+	
+	
+	
+})();
+
